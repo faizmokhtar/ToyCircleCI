0619d75c5c123a2163469a22d50c477e83d0596c
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-02-10 13:37:55 +0800

    Initial Commit

---
+++ ToyCircleCI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToyCircleCI
+//
+//  Created by AD0502-ADE-MB-1 on 10/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToyCircleCIUITests/ToyCircleCIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToyCircleCIUITestsLaunchTests.swift
+//  ToyCircleCIUITests
+//
+//  Created by AD0502-ADE-MB-1 on 10/02/2022.
+//
+
+import XCTest
+
+class ToyCircleCIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
